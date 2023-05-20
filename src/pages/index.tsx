08bc3902cd55c106de2ad824436396e589a6a50e
JoseAle2610 <jalesgervazzi@gmail.com>
2023-05-20 18:15:41 -0400

    with chakra-ui

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@chakra-ui/react'
+
+export default function Home () {
+  return <main className={'hoa'}>
+    <Button colorScheme='teal'>
+       holiii
+    </Button>
+    </main>
+}
